a3c82681e25920134f7e0b402d19630b6a49eac1
Nicholas Ruan <nicholas.ruan.nr@gmail.com>
2019-11-03 10:41:16 -0500

    changed project ids in user schema

--- models/User.js
+++ models/User.js
@@ -24,7 +24,7 @@ const UserSchema = new mongoose.Schema({
 		type: Date,
 		default: Date.now
 	},
-	project_ids: [String]
+	project_ids: [[String]]
 });
 
 module.exports = mongoose.model('User', UserSchema);
